0348e20b1bd31517efae3720069e85839b904513
Alexander Ryzhakov <aryzhakov87@gmail.com>
2025-08-27 10:35:22 +1000

    Error eliminated

--- src/test/java/ru/netology/selenide/CardDeliveryTest.java
+++ src/test/java/ru/netology/selenide/CardDeliveryTest.java
@@ -11,7 +11,6 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.$;
 
 public class CardDeliveryTest {
